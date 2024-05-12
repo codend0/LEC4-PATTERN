7d4efbad7f5cf9afdac6027af3f2e623dc4fe650
ANIKETH SINGH <aniketh9410280466@gmail.com>
2024-05-12 17:20:20 +0530

    Create pattern12.cpp

---
+++ pattern12.cpp
@@ -0,0 +1,27 @@
+/*WE ARE WRITING A CODE FOR PATTERN OF 
+   1
+  22
+ 333
+4444*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int space=n-i;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+        int j=1;
+      while(j<=i){
+          cout<<i;
+          j=j+1;
+      }
+      cout<<endl;
+      i=i+1;
+    }
+}
