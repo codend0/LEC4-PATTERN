c21f4f5ada160bf0df2d3c0cdc1a71773d917c43
ANIKETH SINGH <aniketh9410280466@gmail.com>
2024-05-12 17:06:54 +0530

    Create pattern11.cpp

---
+++ pattern11.cpp
@@ -0,0 +1,27 @@
+/*WE ARE WRITING A CODE FOR PATTERN OF 
+1111
+ 222
+  33
+   4*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int space=i-1;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+        int j=n-i+1;
+      while(j){
+          cout<<i;
+          j=j-1;
+      }
+      cout<<endl;
+      i=i+1;
+    }
+}
