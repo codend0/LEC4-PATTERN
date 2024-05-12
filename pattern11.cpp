0ac661f941256cd6094d1a8fbecd0778af590a52
ANIKETH SINGH <aniketh9410280466@gmail.com>
2024-05-12 17:12:50 +0530

    Update pattern11.cpp

--- pattern11.cpp
+++ pattern11.cpp
@@ -11,15 +11,18 @@ int main(){
     cin>>n;
     int i=1;
     while(i<=n){
+     //loop for space
         int space=i-1;
         while(space){
             cout<<" ";
-            space=space-1;
+            space=space-1;// condition of sapce in decreasing order 
         }
-        int j=n-i+1;
-      while(j){
-          cout<<i;
-          j=j-1;
+     //loop for column
+        int j=n-i+1; // column are decreaing in this pattern so we have to redefine the column 
+     // always check the defination of row nad column if pyramind shape is change for like this case and try to think of similar pattern of star or alphabet maybe
+      while(j){      // justlike we do in star case taking inspiration fro sam e star pattern running loop only for column
+          cout<<i;   // the row no. in the number shwoing in every row so we have to print row no.
+          j=j-1;     // and column should be decrease by 1 
       }
       cout<<endl;
       i=i+1;
